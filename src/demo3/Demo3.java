864d853567d2f188568a237f5430208809353718
khsed <khsed@10.0.0.89>
2023-07-27 22:23:20 -0400

    This is my last project

---
+++ src/demo3/Demo3.java
@@ -0,0 +1,12 @@
+package demo3;
+
+public class Demo3 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("in order to create a local repository, right click on project name, click on team, share project"
+				+ "then click on use repository and click on another option under this and then finish");
+		
+	}
+
+}
